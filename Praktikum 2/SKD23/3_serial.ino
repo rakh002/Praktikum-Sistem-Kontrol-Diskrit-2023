5685f4a11f871d4a5818eb378ece29ea182f0203
Imam Rakhmadi <rakhmadiimam@gmail.com>
2023-10-17 13:44:57 +0700

    Update 3_serial.ino

--- Praktikum 2/SKD23/3_serial.ino
+++ Praktikum 2/SKD23/3_serial.ino
@@ -1,5 +1,6 @@
 int serial(int SerialRead) {
   if (Serial.available() > 0) {
+    Serial.println('\t');
     String StringSerial = Serial.readStringUntil('\n');
     SerialRead = StringSerial.toInt();
     if (SerialRead > 100) return 100;
