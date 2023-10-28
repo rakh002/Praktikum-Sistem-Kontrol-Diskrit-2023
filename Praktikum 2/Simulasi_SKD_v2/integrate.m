279481b615ebba9e5f1e75a70292e32cc39549e5
Imam Rakhmadi <rakhmadiimam@gmail.com>
2023-10-29 03:26:24 +0700

    update position

---
+++ Praktikum 2/Simulasi_SKD_v2/integrate.m
@@ -0,0 +1,3 @@
+function x = integrate(ts,v,xpast)
+    x = xpast + v*ts;
+end
--- Praktikum 2/Simulasi_SKD_v2/simulasi.m
+++ Praktikum 2/Simulasi_SKD_v2/simulasi.m
@@ -31,49 +31,54 @@ clc, clear control delay a2d, warning off
 tr = 1;
 te = 1000;
 v = 0;
+x=0;
 vm = 0;
 imv = 0;
 sp = 75;
 u = sp;
-tdlay = 37;
-gain = 0.839;
-tau = 54;
+tdlay = 42.453;
+gain = 0.925;
+tau = 65.736;
 ts = 10;
-kp = 1.2*tau/gain/tdlay;
-ti = 2*tdlay;
-td = 0.5*tdlay;
-ki = kp/ti;
-kd = kp*td;
+%kp = 1674; % 1.2*tau/gain/tdlay; 
+%ti = 1000000000000000000000000000000000000; %2*tdlay;
+%td = 0; %0.5*tdlay;
+% ki = kp/ti;
+% kd = kp*td;
 
-% P - Ziegler Nichols
-%kp = 1*tau/gain/tdlay;
-%ti = 999999999999999999999999999999999999;
-%td = 0;
-
-% PI - Ziegler Nichols
-%kp = 0.9*tau/gain/tdlay;
-%ti = tdlay/0.3;
-%td = 0;
-
-% PID - Ziegler Nichols
-%kp = 1.2*tau/gain/tdlay;
-%ti = 2*tdlay;
-%td = 0.5*tdlay;
-
-% P - Cohen Coon
-%kp = tau*(1+tdlay/(3*tau))/gain/tdlay;
-%ti = 999999999999999999999999999999999999;
-%td = 0;
-
-% PI - Cohen Coon
-%kp = tau*(0.9+tdlay/(12*tau))/gain/tdlay; 
-%ti = tdlay*(30+3*(tdlay/tau))/(9+20*(tdlay/tau));
-%td = 0;
- 
-% PID - Cohen Coon
-%kp = tau*(4/3+tdlay/(4*tau))/gain/tdlay; 
-%ti = tdlay*(32+6*(tdlay/tau))/(13+8*(tdlay/tau));
-%td = tdlay*4/(11+2*(tdlay/tau));
+% Controller Design : Ziegler Nichols FOPDT
+Type = 4; % 1:P, 2:PI, 3:PID, otherwise:Your Parameter Control
+switch Type
+    case 1
+        % Proportional Controller
+        kp = tau/gain/tdlay;
+        ti = 10000000000000000000000000000000000;
+        td = 0;
+        ki = 0;
+        kd = kp*td;
+    case 2
+        % Proportional–Integral Controller
+        kp = 0.9*tau/gain/tdlay
+        ti = tdlay/0.3
+        ki = kp/ti
+        td = 0;
+        kd = 0;
+    case 3
+        % Proportional–Integral–Derivative Controller
+        kp = 1.2*tau/gain/tdlay
+        ti = 2*tdlay
+        td = 0.5*tdlay
+        ki = kp/ti
+        kd = kp*td
+    otherwise
+        % Custom Control Parameter
+        kp = 1;
+        ti = 1000000000000000000;
+        ki = kp/ti;
+        td = 0.1;
+        ki = 0;
+        kd = 0.1;
+end
 
 sampling = true;
 t_dis_ol = [];
@@ -83,9 +88,11 @@ vm_dis_ol = [];
 t_con_ol = [];
 du_con_ol = [];
 v_con_ol = [];
+x=0;
 
 %% open loop
 for t=0:tr:te
+    
     if sampling
         iu = d2a(u);
         t_dis_ol(end+1) = t;
@@ -95,14 +102,16 @@ for t=0:tr:te
 
     du = delay(iu,t,tr,tdlay);
     v = first_order_process(tr,v,du,gain,tau);
-    [vm,sampling] = a2d(t,ts,v);
+    x = integrate(ts,v,x);
+    [vm,sampling] = a2d(t,ts,x);
 
     t_con_ol(end+1) = t;
     du_con_ol(end+1) = du;
-    v_con_ol(end+1) = v;
+    v_con_ol(end+1) = x;
 end
 
 clear delay a2d
+x=0;
 v = 0;
 vm = 0;
 sampling = true;
@@ -128,15 +137,16 @@ for t=0:tr:te
 
     dmv = delay(imv,t,tr,tdlay);
     v = first_order_process(tr,v,dmv,gain,tau);
-    [vm,sampling] = a2d(t,ts,v);
+    x = integrate(ts,v,x);
+    [vm,sampling] = a2d(t,ts,x);
 
     t_con_cl(end+1) = t;
     dmv_con_cl(end+1) = dmv;
-    v_con_cl(end+1) = v;
+    v_con_cl(end+1) = x;
 end
 
 %% Grafik
-figure(1)
+figure(2)
 hold off
 plot([t_con_ol(1) t_con_ol(end)],[u u],'r-.')
 hold on
@@ -146,17 +156,17 @@ stairs(t_dis_ol,vm_dis_ol,'b-')
 stairs(t_dis_cl,vm_dis_cl,'g-')
 legend('set point/input','open loop','closed loop')
 
-figure(2)
+figure(3)
 hold off
 plot([t_con_ol(1) t_con_ol(end)],[sp sp],'r-.')
 hold on
 plot(t_con_cl,v_con_cl,'g--')
-stairs(t_dis_cl,vm_dis_cl,'g-') 
+stairs(t_dis_cl,vm_dis_cl,'g-')
 stairs(t_dis_cl,imv_dis_cl,'m-')
 plot(t_con_cl,dmv_con_cl,'c--')
 legend('set point','output','output measured','input','delayed input')
 
-figure(3)
+figure(4)
 hold off
 stairs(t_dis_cl(2:end),mv_dis_cl(2:end),'k-.')
 hold on
